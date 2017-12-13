9b0f50a6e0de75495142d8a15fc4116f19d4d0a1
rajasekar <rajasekar@bluetalon.com>
2017-12-13 17:16:02 +0530

    initial state

--- src/components/table.js
+++ src/components/table.js
@@ -1,37 +1,6 @@
 import React,{Component} from 'react';
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
-
-/**
- * 
- * props- > 
- * 
- *  columns : {
- *      [ 
- *          {
- *              field : 'fieldName',
- *              label : 'FieldName,
- *              render : (data, row)
- *          },
- *          {
- *              field : 'fieldName1',
- *              label : 'FieldName,
- *              render : (data, row)
- *          },
- *      ]
- *  } 
- * 
- * data : [ {
- *      fieldName : 'fieldValue00',
- *      fieldName1 : 'fieldValue10
- *  },
- * {
- *      fieldName: 'fieldValue11',
- *      fieldName : 'fieldValue20
- * }]
- */
-
-
  let columns = [
      {
          field : 'firstName',
@@ -71,99 +40,69 @@ import { Icon, Label, Menu, Table } from 'semantic-ui-react'
          lastName: 'bacon',
          age: 59
      }
- ]
+ ];
 
- let pageSize = 10;
-
- var record = i => {
-     return {    
-        firstName: 'firstName' + i,
-        lastName: 'lastName' + i,
-        age: i        
-     }
- }
- 
- data = new  Array(100).fill(0).map((d,i) => record(i));
-
-class PageHandler {
-    constructor(pageSize, data) {
-        this.pages = 0;
-        this.pageSize = pageSize;
-        this.data = data;
-    }
-
-    getPage(pageNo) {
-
-    }
-
-
-    getPages() {      
-        return (this.data.length / this.pageSize) + 1
-    }
-}
-
-var mapFromNum = (n, fn) => new Array(n).fill(0).map((d,i) => fn(i));
+var mapTimes = (n, fn) => new Array(n).fill(0).map((d,i) => fn(i));
 
 class MyTable extends Component{
     constructor(props) {
         super(props);
-        this.pagedData = new PageHandler(20, data);
         this.state = {
             currentPage : 1,
             data : data,
-            columnInfo : columns,
-            pages: this.pagedData.getPages()
+            columnInfo : columns,            
         }
-    }
- 
+    } 
 
     render(){
         return (
             <div className='formContainer'>
-                <Table celled >
+                <Table celled>
                     <Table.Header>
-                    <Table.Row>            
-                        {this.state.columnInfo.map((d,i) => <Table.HeaderCell>{d.header}</Table.HeaderCell>)}
+                    <Table.Row>
+                        <Table.HeaderCell>Header</Table.HeaderCell>
+                        <Table.HeaderCell>Header</Table.HeaderCell>
+                        <Table.HeaderCell>Header</Table.HeaderCell>
                     </Table.Row>
-                    </Table.Header>           
+                    </Table.Header>
+
                     <Table.Body>
-                        {
-                            this.state.data.map((d) => {
-                                return (
-                                    <Table.Row>
-                                        {/* {columns.map((c) => <Table.Cell>{render(d, c)}</Table.Cell>)} */}
-                                        {
-                                            columns.map((c) => <Table.Cell>{d[c.field]}</Table.Cell>)
-                                        }
-                                    </Table.Row>    
-                                );
-                            })
-                        }
+                    <Table.Row>
+                        <Table.Cell>
+                        <Label ribbon>First</Label>
+                        </Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                    </Table.Row>
                     </Table.Body>
+
                     <Table.Footer>
-                        <Table.Row>
-                            <Table.HeaderCell colSpan='3'>
-                            <Menu pagination floated='right' >
-                                {
-                                    this.state.pages > 1 &&
-                                    <Menu.Item as='a' icon>
-                                        <Icon name='left chevron' />
-                                    </Menu.Item>
-                                }
-                                {
-                                    mapFromNum(this.state.pages, (i) => 
-                                        <Menu.Item as='a' tag={i+1} active={this.state.currentPage == i + 1} onClick={(e, data) => this.pageChanged(parseInt(data.tag))}>{i +1}</Menu.Item>
-                                        )
-                                }
-                                { 
-                                    this.state.pages > 1 && 
-                                    <Menu.Item as='a' icon>
-                                        <Icon name='right chevron' />
-                                    </Menu.Item>
-                                }
-                            </Menu>
-                            </Table.HeaderCell>
-                        </Table.Row>
+                    <Table.Row>
+                        <Table.HeaderCell colSpan='3'>
+                        <Menu floated='right' pagination>
+                            <Menu.Item as='a' icon>
+                            <Icon name='left chevron' />
+                            </Menu.Item>
+                            <Menu.Item as='a'>1</Menu.Item>
+                            <Menu.Item as='a'>2</Menu.Item>
+                            <Menu.Item as='a'>3</Menu.Item>
+                            <Menu.Item as='a'>4</Menu.Item>
+                            <Menu.Item as='a' icon>
+                            <Icon name='right chevron' />
+                            </Menu.Item>
+                        </Menu>
+                        </Table.HeaderCell>
+                    </Table.Row>
                     </Table.Footer>
                 </Table>
             </div>
@@ -176,16 +115,16 @@ class MyTable extends Component{
 }
 
 
-function render(row, columnInfo) {
+/*function render(row, columnInfo) {
     //debugger;
     var field = columnInfo.field;
     /*if(columnInfo.render) {
         return columnInfo.render(row[field], row);
     }else {
         return row[field];
-    }*/
+    }
 
     return row[field];
-}
+}*/
 
 export default MyTable
