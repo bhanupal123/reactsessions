eec0d8b5026d3c58f7a862534f13117efd389319
rajasekar <rajasekar@bluetalon.com>
2017-12-13 17:46:59 +0530

    More changes

--- src/components/table.js
+++ src/components/table.js
@@ -1,60 +1,16 @@
 import React,{Component} from 'react';
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
- let columns = [
-     {
-         field : 'firstName',
-         header: 'First Name',
-         render : (data, row) => {
-            return (
-               <Label>
-                   <Icon name='user' /> {data}
-               </Label>
-            );
-        }
-     },
-     {
-         field : 'lastName',
-         header: 'Last Name'
-     },
-     {
-         field : 'age',
-         header : 'Age',         
-     }
- ]
-
-
- let data = [
-     {
-         firstName : 'eric',
-         lastName : 'banna',
-         age: 46
-     },
-     {
-         firstName: 'megan',
-         lastName: 'fox',
-         age: 26
-     },
-     {
-         firstName: 'kevin',
-         lastName: 'bacon',
-         age: 59
-     }
- ];
 
 var mapTimes = (n, fn) => new Array(n).fill(0).map((d,i) => fn(i));
 
 class MyTable extends Component{
     constructor(props) {
-        super(props);
-        this.state = {
-            currentPage : 1,
-            data : data,
-            columnInfo : columns,            
-        }
+        super(props);        
     } 
 
     render(){
+        console.log(this.props);
         return (
             <div className='formContainer'>
                 <Table celled>
@@ -68,9 +24,7 @@ class MyTable extends Component{
 
                     <Table.Body>
                     <Table.Row>
-                        <Table.Cell>
-                        <Label ribbon>First</Label>
-                        </Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
                         <Table.Cell>Cell</Table.Cell>
                         <Table.Cell>Cell</Table.Cell>
                     </Table.Row>
@@ -85,19 +39,18 @@ class MyTable extends Component{
                         <Table.Cell>Cell</Table.Cell>
                     </Table.Row>
                     </Table.Body>
-
                     <Table.Footer>
                     <Table.Row>
                         <Table.HeaderCell colSpan='3'>
                         <Menu floated='right' pagination>
-                            <Menu.Item as='a' icon>
+                            <Menu.Item as='a' tag="-1" icon>
                             <Icon name='left chevron' />
                             </Menu.Item>
-                            <Menu.Item as='a'>1</Menu.Item>
-                            <Menu.Item as='a'>2</Menu.Item>
-                            <Menu.Item as='a'>3</Menu.Item>
-                            <Menu.Item as='a'>4</Menu.Item>
-                            <Menu.Item as='a' icon>
+                            <Menu.Item as='a' tag="1">1</Menu.Item>
+                            <Menu.Item as='a' tag="2">2</Menu.Item>
+                            <Menu.Item as='a' tag="3">3</Menu.Item>
+                            <Menu.Item as='a' tag="4">4</Menu.Item>
+                            <Menu.Item as='a' tag="+1" icon>
                             <Icon name='right chevron' />
                             </Menu.Item>
                         </Menu>
@@ -113,18 +66,5 @@ class MyTable extends Component{
         console.log(`page changed to ${pageNo}`);
     }
 }
-
-
-/*function render(row, columnInfo) {
-    //debugger;
-    var field = columnInfo.field;
-    /*if(columnInfo.render) {
-        return columnInfo.render(row[field], row);
-    }else {
-        return row[field];
-    }
-
-    return row[field];
-}*/
 
 export default MyTable
